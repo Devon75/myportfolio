5e96da1ee18cd9d02dba5b73b4045d92d9249023
joris <jorisobiang306@gmail.com>
2024-09-19 18:56:28 +0200

    commit 19-09-2024 à 18h56

--- src/components/Accueil.jsx
+++ src/components/Accueil.jsx
@@ -5,13 +5,13 @@ function Accueil() {
         <section class="flex justify-center items-center text-center h-screen bg-gradient-to-tr from-lime-50 to-emerald-50 dark:bg-gray-900">
             <div class=" mt-10">
                 <div class="ml-28 sm:ml-16 minus:ml-5 minus:mr-5 mr-16 sm:mr-16">
-                    <a href="#" class="inline-flex justify-between items-center py-1 px-1 pl-4 pr-4 mb-7 text-sm text-emerald-950 bg-none rounded-full dark:bg-gray-800 dark:text-white hover:bg-blanclune-100 hover:text-teal-900 dark:hover:bg-gray-700" role="alert">
-                        <span class="text-sm font-medium">Software & backend developper</span>
+                    <Link to="/Projet" class="inline-flex justify-between items-center py-1 px-1 pl-4 pr-4 mb-7 text-sm text-emerald-950 bg-none rounded-full dark:bg-gray-800 dark:text-white hover:bg-blanclune-100 hover:text-teal-900 dark:hover:bg-gray-700" role="alert">
+                        <span class="text-sm font-medium">créateur de site web fullstack</span>
                         <svg class="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
-                    </a>
+                    </Link>
                     <h1 class="max-w-2xl mb-4 mr-5 text-4xl text-emerald-950 text-center font-bold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Développeur web fullstack</h1>
                     <p class="max-w-2xl mb-6 font-light text-emerald-950 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem officiis pariatur recusandae aliquam quam, nostrum hic quia! Ipsum odio at, reprehenderit rerum quidem commodi, earum dolorem nulla hic ducimus fugiat.</p>
-                    <a href="src/image/Développeur_web_full-stack.pdf" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-emerald-950 border border-gray-300 rounded-lg hover:bg-emerald-50 hover:text-teal-900 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+                    <a href="src/assets/Développeur_web_full-stack.pdf" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-emerald-950 border border-gray-300 rounded-lg hover:bg-emerald-50 hover:text-teal-900 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
                         Télécharger mon CV
                     </a>
                 </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,13 @@
-import { Outlet,Link } from "react-router-dom";
-
-
-
+import { Link, Outlet } from "react-router-dom";
 
 function Navbar() {
 
-
     return (
         <>
-            <nav class="h-0 border-gray-200 dark:bg-gray-900 drop-shadow-2xl">
-                <div class="h-0 max-w-screen-xl flex flex-wrap justify-between mx-0">
-                    <Link to="/" class="flex ml-28 space-x-3 rtl:space-x-reverse minus:ml-5 sm:ml-16">
-                        <img src="src/image/logo.png" class="w-8 mt-4 h-8" alt="Flowbite Logo" />
+            <nav class="min-h-20 flex items-center justify-center bg-gradient-to-tr from-emerald-50 to-lime-50 border-gray-200 dark:bg-gray-900">
+                <div class="w-2/5 items-start justify-center ">
+                    <Link to="/" class="flex items-baseline justify-center space-x-3 rtl:space-x-reverse minus:ml-5 sm:ml-16">
+                        <img src="src/assets/image/newlogo.png" alt="Flowbite Logo" class="w-28 h-28" />
                     </Link>
                     <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg sm:mr-16 md:hidden mt-10 mr-4 laptop:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
                         <span class="sr-only">Open main menu</span>
@@ -19,23 +15,24 @@ function Navbar() {
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
                         </svg>
                     </button>
-                    <div class="w-1/2 h-0 minus:hidden sm:hidden md:block md:w-max" id="navbar-default">
-                        <ul class="font-medium flex flex-row justify-end p-4 mr-10 gap-2 md: bg-transparent mb-5 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md: dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                            <li>
-                                <Link to="/" class="block py-2 px-3 rounded bg-emerald-950 text-white hover:text-white md:text-white md:p-1 dark:text-white md:dark:text-white hover:text-white-100" aria-current="page">Accueil</Link>
-                            </li>
-                            <li>
-                                <Link to="/moi" class="block py-2 px-3 text-emerald-950 rounded hover:bg-blanclune-100 hover:text-emerald-400 md:border-0 md:hover:text-gray-700 md:pt-1 pb-1 pl-2 pr-2 dark:text-white md:dark:hover:text-teal-700 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">A propos de moi</Link>
-                            </li>
-                            <li>
-                                <Link to="/Cv" class="block py-2 px-3 text-emerald-950 rounded hover:bg-blanclune-100 hover:text-emerald-400 md:border-0 md:hover:text-gray-700 md:pt-1 pb-1 pl-2 pr-2 dark:text-white md:dark:hover:text-teal-700 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">CV</Link>
-                            </li>
-                            <li>
-                                <Link to="/Projet" class="block py-2 px-3 text-emerald-950 rounded hover:bg-blanclune-100 hover:text-emerald-400 md:border-0 md:hover:text-gray-700 md:pt-1 pb-1 pl-2 pr-2 dark:text-white md:dark:hover:text-teal-700 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Projet</Link>
-                            </li>
-                        </ul>
-                    </div>
                 </div>
+                <div class="w-3/5 items-center justify-center minus:hidden sm:hidden md:block md:w-max" id="navbar-default">
+                    <ul class="flex items-center mx-60 justify-around md: bg-transparent  md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md: dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                        <li>
+                            <Link to="/" class="block rounded bg-emerald-950 text-white pt-2 pb-2 pl-1 pr-1 hover:text-white md:text-white md:p-1 dark:text-white md:dark:text-white hover:text-white-100" aria-current="page">Accueil</Link>
+                        </li>
+                        <li>
+                            <Link to="/moi" class="block  text-emerald-950 rounded hover:bg-blanclune-100 hover:text-emerald-400 md:border-0 md:hover:text-gray-700 md:pt-1 pb-1 pl-2 pr-2 dark:text-white md:dark:hover:text-teal-700 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">A propos de moi</Link>
+                        </li>
+                        <li>
+                            <Link to="/Cv" class="block  text-emerald-950 rounded hover:bg-blanclune-100 hover:text-emerald-400 md:border-0 md:hover:text-gray-700 md:pt-1 pb-1 pl-2 pr-2 dark:text-white md:dark:hover:text-teal-700 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">CV</Link>
+                        </li>
+                        <li>
+                            <Link to="/Projet" class="block text-emerald-950 rounded hover:bg-blanclune-100 hover:text-emerald-400 md:border-0 md:hover:text-gray-700 md:pt-1 pb-1 pl-2 pr-2 dark:text-white md:dark:hover:text-teal-700 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Projet</Link>
+                        </li>
+                    </ul>
+                </div>
+
             </nav>
 
             <Outlet />
